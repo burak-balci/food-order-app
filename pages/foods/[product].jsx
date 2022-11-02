591d0abc3dca14ec1e7491b34a707596903f9672
Burak <96511762+burak-balci@users.noreply.github.com>
2022-11-02 15:58:47 +0300

    foods page is done

--- pages/foods/[product].jsx
+++ pages/foods/[product].jsx
@@ -2,25 +2,28 @@ import React from "react";
 import { useRouter } from "next/router";
 import styles from "../../styles/ProductDetail.module.css";
 
-const ProductDetail = () => {
+const ProductDetail = ({ data }) => {
   const route = useRouter();
+  const filtered = data.filter((item) => item.title === route.query.product);
 
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
         <img
-          src="https://www.donaldrussell.com/media/catalog/product/q/1/q177.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700"
-          alt=""
+          src={filtered[0].image}
+          alt={filtered[0].title}
           className={styles.image}
         />
       </div>
       <div className={styles.infoContainer}>
-        <span className={styles.title}>Bramley Apple Pies</span>
-        <span className={styles.subTitle}>Classic British dessert</span>
+        <span className={styles.title}>{filtered[0].title}</span>
+        <span className={styles.subTitle}>{filtered[0].subTitle}</span>
         <ul className={styles.list}>
-          <li className={styles.listItem}>British Bramley apples</li>
-          <li className={styles.listItem}>Crisp butter pastry</li>
-          <li className={styles.listItem}>Individual servings</li>
+          {filtered[0].desc.map((item, index) => (
+            <li key={index} className={styles.listItem}>
+              {item}
+            </li>
+          ))}
         </ul>
         <div>
           <span className={styles.listItem}>Quantity : </span>
@@ -33,12 +36,40 @@ const ProductDetail = () => {
           </select>
         </div>
         <div className={styles.price}>
-          <span className={styles.listItem}>Price : </span> $ 44
+          <span className={styles.listItem}>Price : </span> ${" "}
+          {filtered[0].price}
         </div>
         <button className={styles.button}>Add to Cart</button>
       </div>
     </div>
   );
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`http://localhost:3000/api/products`);
+  const data = await res.json();
+
+  const paths = data.map((item) => {
+    return {
+      params: { product: item.title },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(`http://localhost:3000/api/products`);
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default ProductDetail;
--- pages/foods/index.jsx
+++ pages/foods/index.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import Card from "../../components/Card";
 import styles from "../../styles/Foods.module.css";
 
-const Foods = () => {
+const Foods = ({ data }) => {
+  const filtered = data.filter((item) => item.type === "food");
   return (
     <div className={styles.container}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {filtered.map((item) => (
+        <Card key={item._id} item={item} />
+      ))}
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:3000/api/products");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default Foods;
